c595e7676690e1eedf9d5816e0ea84e300021c34
Sam <samuel.sarmiento.1997@gmail.com>
2021-06-22 22:34:52 -0400

    initial commit w/ small description of game

---
+++ blackjack.py
@@ -0,0 +1,6 @@
+'''
+Interactive black jack game. One computer dealer and one human player. Human player will have a starting bankroll.
+The player places a bet and then cards are dealt. One dealer card will be face up (the other face down).
+Player has option of either 'hit' or 'stay'. If player goes over 21 dealer wins. If player stays, dealer will hit
+until they beat player or bust. If player wins their bet gets doubled.
+'''
